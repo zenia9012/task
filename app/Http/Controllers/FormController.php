163d4b0e537a21a31392047fcabffe9fb54bcb74
Yevhenii Riabyi <zenia9012@gmail.com>
2018-04-13 14:59:25 +0300

    validate and save data from form

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class FormController extends Controller
 {
 	public function form( Request $request ) {
 
-		return back();
+		$this->validate($request, [
+			'first_name' => 'required',
+			'last_name' => 'required',
+			'email' => 'required|email',
+			'password' => 'required|min:6|confirmed',
+		]);
+
+		$firstName = $request->input('first_name');
+		$lastName = $request->input('last_name');
+		$email = $request->input('email');
+		$password = Hash::make($request->input('password'));
+
+		$referCode = strtolower(str_random(6));
+
+		Client::create($firstName, $lastName, $email, $password, $referCode);
+
+		return back()->with('message', 'Registration was success');
     }
 }
