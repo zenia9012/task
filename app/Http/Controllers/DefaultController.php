e52c0bb6c6aec6b9940a6cb63e734d330714a9ef
Yevhenii Riabyi <zenia9012@gmail.com>
2018-04-13 15:36:39 +0300

    check if get param have ref key

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -3,17 +3,40 @@
 namespace App\Http\Controllers;
 
 
+use App\Manager\ReferManager;
+use App\Models\Refer;
 use Illuminate\Http\Request;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+	/*
+	 * main page controller
+	 */
 	public function index( Request $request ) {
 
-		return view('index.main');
+		$refer = ReferManager::existsReferCode( $request );
+
+		if ( $refer ) {
+			Refer::create( $request );
+
+			return redirect(route('main'));
+		}
+
+		return view( 'index.main' );
 	}
 
+	/*
+	 * news page controller
+	 */
 	public function news( Request $request ) {
 
-		return view('index.news');
+		$refer = ReferManager::existsReferCode( $request );
+
+		if ( $refer ) {
+			Refer::create( $request );
+
+			return redirect(route('news'));
+		}
+
+		return view( 'index.news' );
 	}
 }
